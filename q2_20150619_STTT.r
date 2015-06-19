642388b4fdbc9db044d45925e12ed20c36e7d374
SaiTheinThanTun <driftwood711@gmail.com>
2015-06-19 10:48:25 +0700

    tweaked q2 for 2014

--- q2_20150619_STTT.r
+++ q2_20150619_STTT.r
@@ -5,7 +5,7 @@ setwd("C:/WorkingDirectoryName") #The name and path can be anywhere in your comp
 #2. MARC PCodes.csv ##Link: https://app.box.com/s/4jwka23zsbsukxtng1c8t6jq9vpxayy1
 
 #The outputs will also be produced in the same directory.
-#Outputs are for 2013 MARC area: 
+#Outputs are for 2014 MARC area: 
 #1. Plot1: CHW histogram for average RDTs: Normal
 #2. Plot2: CHW histogram for average RDTs:base2
 #3. Plot3: Testing per IP, boxplot with outliers
@@ -23,8 +23,8 @@ q2 <- read.csv("Q2.csv")
 q2$Month[q2$Month=="April"] <- "Apr" #remove after being cleaned
 q2$Month[q2$Month=="July"] <- "Jul"
 
-#Subsetting for 2013 MARC, #and outcomes
-q2 <- q2[q2$Year=="2013",]
+#Subsetting for 2014 MARC, #and outcomes
+q2 <- q2[q2$Year=="2014",]
 marc_p <- readLines("MARC PCodes.csv")
 q2 <- q2[q2$TS_Pcode %in% marc_p[-1],]
 q2 <- q2[q2$Volunteer.Villages!="",]
@@ -60,31 +60,31 @@ mean_rdt <- mean(uniq_villages$CountOfOutcome) #8.28854
 #1. Plot1: CHW histogram for average RDTs: Normal
 uniq_villages$f <- cut(uniq_villages$CountOfOutcome, c(0,10,20,30,40,50,250), labels=c("<=10","11-20","21-30","31-40","41-50",">50"))
 png(file=paste("chw_hist_normal_",Sys.Date(),".png",sep=""))
-barplot(table(uniq_villages$f), main=paste("Average malaria testing rates of \nCommunity Health Workers per month in MARC area, 2013 \n(median=",round(med_rdt,1),")",sep=""), xlab= "No. of malaria tests", ylab= "No. of Community Health Workers")
+barplot(table(uniq_villages$f), main=paste("Average malaria testing rates of \nCommunity Health Workers per month in MARC area, 2014 \n(median=",round(med_rdt,1),")",sep=""), xlab= "No. of malaria tests", ylab= "No. of Community Health Workers")
 dev.off()
 
 #2. Plot2: CHW histogram for average RDTs:base2
 uniq_villages$f <- cut(uniq_villages$CountOfOutcome, c(0,2^(0:6)[-1],250), labels=c("<=2","3-4","5-8","9-16","17-32","33-64",">64"))
 png(file=paste("chw_hist_base2_",Sys.Date(),".png",sep=""))
-barplot(table(uniq_villages$f), main=paste("Average malaria testing rates of \nCommunity Health Workers per month in MARC area, 2013 \n(median=",round(med_rdt,1),")",sep=""), xlab= "No. of malaria tests", ylab= "No. of Community Health Workers")
+barplot(table(uniq_villages$f), main=paste("Average malaria testing rates of \nCommunity Health Workers per month in MARC area, 2014 \n(median=",round(med_rdt,1),")",sep=""), xlab= "No. of malaria tests", ylab= "No. of Community Health Workers")
 dev.off()
 
 #3. Plot3: Testing per IP, boxplot with outliers
-png(file=paste("boxplot_IP_outliers_",Sys.Date(),".png",sep=""))
-boxplot(CountOfOutcome ~ Source,uniq_villages, xlab="Implementing partners", ylab="Malaria tests", main="Malaria testing rates \n among Implementing Partners, MARC area (2013)") 
+png(file=paste("boxplot_IP_outliers_",Sys.Date(),".png",sep=""),width=960, height=960)
+boxplot(CountOfOutcome ~ Source,uniq_villages, xlab="Implementing partners", ylab="Malaria tests", main="Malaria testing rates \n among Implementing Partners, MARC area (2014)") 
 dev.off()
 #4. Plot4: Testing per IP, boxplot no outliers
-png(file=paste("boxplot_IP_",Sys.Date(),".png",sep=""))
-boxplot(CountOfOutcome ~ Source,uniq_villages, outline=FALSE, xlab="Implementing partners", ylab="Malaria tests", main="Malaria testing rates \n among Implementing Partners, MARC area (2013)") 
+png(file=paste("boxplot_IP_",Sys.Date(),".png",sep=""),width=960, height=960)
+boxplot(CountOfOutcome ~ Source,uniq_villages, outline=FALSE, xlab="Implementing partners", ylab="Malaria tests", main="Malaria testing rates \n among Implementing Partners, MARC area (2014)") 
 dev.off()
 
 #5. Plot5: Testing per State/Division, boxplot with outliers
 png(file=paste("boxplot_states_outliers_",Sys.Date(),".png",sep=""), width=640)
-boxplot(CountOfOutcome ~ MaxOfState..Division,uniq_villages, xlab="States/Regions", ylab="Malaria tests", main="Malaria testing rates \n across States and Regions \n MARC area (2013)", cex.names=.8) 
+boxplot(CountOfOutcome ~ MaxOfState..Division,uniq_villages, xlab="States/Regions", ylab="Malaria tests", main="Malaria testing rates \n across States and Regions \n MARC area (2014)", cex.names=.8) 
 dev.off()
 #6. Plot6: Testing per State/Division, boxplot no outliers
 png(file=paste("boxplot_states_",Sys.Date(),".png",sep=""), width=640)
-boxplot(CountOfOutcome ~ MaxOfState..Division,uniq_villages, xlab="States/Regions", ylab="Malaria tests", main="Malaria testing rates \n across States and Regions \n MARC area (2013)", outline=FALSE, cex.names=.8) 
+boxplot(CountOfOutcome ~ MaxOfState..Division,uniq_villages, xlab="States/Regions", ylab="Malaria tests", main="Malaria testing rates \n across States and Regions \n MARC area (2014)", outline=FALSE, cex.names=.8) 
 dev.off()
 
 #7. Table1: CHW count per [Township,IP] #Assuming that each village has a volunteer
@@ -94,7 +94,7 @@ write.csv(chw_tsp_ip, paste("CHW_count_tsp_IP_", Sys.Date(),".csv",sep=""))
 
 #8. Table2: Number of CHW test per [Township, IP] #Assuming that each village has a volunteer
 chwtst_tsp_ip <- dcast(uniq_villages, TS_Pcode+MaxOfTownship ~ Source, sum, value.var="CountOfOutcome")
-chwtst_tsp_ip[,3:12] <- round(chwtst_tsp_ip, digits=0)
+chwtst_tsp_ip[,3:13] <- round(chwtst_tsp_ip[,3:13], digits=0)
 write.csv(chwtst_tsp_ip, paste("CHW_test_tsp_IP_", Sys.Date(),".csv",sep=""))
 
 
